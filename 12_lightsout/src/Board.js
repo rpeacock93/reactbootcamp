6076d0d1e3e6bd1d4c5a278dec836e0a09857e73
Ryan Peacock <rpeacock@learn4life.org>
2019-04-29 14:55:21 -0700

    lights out finished

--- 12_lightsout/src/Board.js
+++ 12_lightsout/src/Board.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Cell from "./Cell";
-import './Board.css';
-
+import "./Board.css";
 
 /** Game board of Lights out.
  *
@@ -34,7 +33,7 @@ class Board extends Component {
     nrows: 5,
     ncols: 5,
     chanceLightStartsOn: 0.25
-  }
+  };
   constructor(props) {
     super(props);
 
@@ -42,7 +41,7 @@ class Board extends Component {
     this.state = {
       hasWon: false,
       board: this.createBoard()
-    }
+    };
   }
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
@@ -51,23 +50,21 @@ class Board extends Component {
     let board = [];
     // TODO: create array-of-arrays of true/false values
     for (let y = 0; y < this.props.nrows; y++) {
-      let row = []
+      let row = [];
       for (let x = 0; x < this.props.ncols; x++) {
-        row.push(Math.random() < this.props.chanceLightStartsOn)
+        row.push(Math.random() < this.props.chanceLightStartsOn);
       }
-      board.push(row)
+      board.push(row);
     }
-    return board
+    return board;
   }
 
   /** handle changing a cell: update board & determine if winner */
 
   flipCellsAround(coord) {
-    console.log('flipping', coord)
     let { ncols, nrows } = this.props;
     let board = this.state.board;
     let [y, x] = coord.split("-").map(Number);
-
 
     function flipCell(y, x) {
       // if this coord is actually on board, flip it
@@ -76,60 +73,63 @@ class Board extends Component {
         board[y][x] = !board[y][x];
       }
     }
-
-    // Flip initial cell
-    flipCell(y, x)
-    flipCell(y, x - 1)
-    flipCell(y, x + 1)
-    flipCell(y + 1, x)
-    flipCell(y - 1, x)
-
     // TODO: flip this cell and the cells around it
+    flipCell(y, x); //Flip initial cell
+    flipCell(y, x - 1); //flip left
+    flipCell(y, x + 1); //flip right
+    flipCell(y - 1, x); //flip below
+    flipCell(y + 1, x); //flip above
 
     // win when every cell is turned off
     // TODO: determine is the game has been won
-
-    let hasWon = board.every(row => row.every(cell => !cell))
+    let hasWon = board.every(row => row.every(cell => !cell));
 
     this.setState({ board: board, hasWon: hasWon });
   }
 
-
   /** Render game board or winning message. */
-
-  render() {
-    if(this.state.hasWon) {
-      return <h1>YOU WON!!</h1>
-    }
-    // if the game is won, just show a winning msg & render nothing else
-
-    // TODO
-
-    // make table board
-
-    // TODO
+  makeTable() {
     let tblBoard = [];
     for (let y = 0; y < this.props.nrows; y++) {
       let row = [];
       for (let x = 0; x < this.props.ncols; x++) {
-        let coord = `${y}-${x}`
-        row.push(<Cell key={coord} isLit={this.state.board[y][x]}
-          flipCellsAroundMe={() => this.flipCellsAround(coord)}
-        />)
+        let coord = `${y}-${x}`;
+        row.push(
+          <Cell
+            key={coord}
+            isLit={this.state.board[y][x]}
+            flipCellsAroundMe={() => this.flipCellsAround(coord)}
+          />
+        );
       }
-      tblBoard.push(<tr key={y}>{row}</tr>)
+      tblBoard.push(<tr key={y}>{row}</tr>);
     }
     return (
+      <table className='Board'>
+        <tbody>{tblBoard}</tbody>
+      </table>
+    );
+  }
+  render() {
+    return (
       <div>
-        <table className="Board">
-          <tbody>
-            {tblBoard}
-          </tbody>
-        </table>
+        {this.state.hasWon ? (
+          <div className='winner'>
+            <span className='neon-orange'>YOU</span>
+            <span className='neon-blue'>WIN!</span>
+          </div>
+        ) : (
+            <div>
+              <div className='Board-title'>
+                <div className='neon-orange'>Lights</div>
+                <div className='neon-blue'>Out</div>
+              </div>
+              {this.makeTable()}
+            </div>
+          )}
       </div>
-    )
+    );
   }
 }
-
 
 export default Board;
